eddf0181923672102328a174466dcb74cf291743
wangjiankui <952035865@qq.com>
2016-04-27 17:31:34 +0800

    添加gulp-sourcemaps

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -5,45 +5,46 @@
 "use strict";
 
 var gulp         = require('gulp');
-var gulp_postcss = require('gulp-postcss');
 var browserSync = require('browser-sync').create();
-var autoprefixer = require('autoprefixer');
 var rename       = require('gulp-rename');
-var csswring     = require('csswring');
 var concat       = require('gulp-concat');
-var px2rem       = require("postcss-px2rem");
+var px3rem       = require("gulp-px3rem");
 var sass         = require('gulp-sass');
 var uglify 		  = require("gulp-uglify");
-gulp.task('build:css', function(){
-	var processors = [
-		autoprefixer,
-		px2rem({ remUnit: 72 }),
-		csswring({
-			preserveHacks: true,
-			removeAllComments: true
-		})
-	];
-	return gulp.src(['css/sass/*.scss','!css/sass/base.scss'])
-		.pipe(sass())//编译scss
-		.pipe(gulp.dest('css/style'))//编译好的sass输出到css下的style文件下
-		.pipe(gulp_postcss(processors))
-		.pipe(concat('main.css'))//合并为main.css
-		.on('error', errorHandler)
-		.pipe(rename({suffix: ".min"}))//重命名为main.min.css
-		.pipe(gulp.dest('./css'))//输出在css文件里
-		.pipe(browserSync.stream())//自动刷新浏览器
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCss = require("gulp-minify-css");
+
+
+//编译sass，标记maps文件
+gulp.task("sass",function(){
+	return  gulp.src(['css/sass/*.scss'])
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('./css/style'))
+			.pipe(concat('main.css'))
+			.pipe(sourcemaps.write('./maps'))
+			.pipe(gulp.dest('./css'));
+});
+//px转换rem
+gulp.task('px3rem',["sass"],function(){
+	gulp.src('css/main.css')
+			.pipe(px3rem({remUnit: 72 }))//转化基值72，不写默认为75
+			.pipe(minifyCss())
+			.pipe(rename('main.min.css'))//重命名文件
+			.pipe(gulp.dest('./css'))
 });
 
+
 //js压缩
-
-
 gulp.task('minify-js', function () {
 	gulp.src('js/src/*.js') // 要压缩的js文件
 		.pipe(uglify())//使用uglify进行压缩,更多配置请参考：
 		.pipe(rename({suffix:".min"}))
 		.pipe(gulp.dest('js/minJs')); //压缩后的路径
 });
- //Static server
+
+
+//Static server
 gulp.task('browser-sync', function() {
 	var files = [
 		'pages/*.html',
@@ -52,22 +53,21 @@ gulp.task('browser-sync', function() {
 	];
 	browserSync.init(files,{
 		server: {
-			baseDir: "/"//定义从那个位置启动服务器
+			baseDir: "//"//定义从那个位置启动服务器
 		}
 	});
 });
 gulp.task('watch', function () {
 	gulp.watch([
 		'css/sass/*.scss',
-		'!css/main.min.css',
 		'pages/*.html',
 		'js/src/*.js'
-	], ['build:css','minify-js']);
+	], ['minify-js','sass','px3rem']);
 });
 
 
-
-gulp.task('default', ['watch','browser-sync']);
+//,'browser-sync'
+gulp.task('default', ['watch']);
 function errorHandler(error){
 	this.emit('end');
 }
